57429e8893d9e222dde956ba826f4008abce3dfc
accountforfun <fabio.calzavara@itsrizzoli.it>
2024-09-20 13:42:43 +0000

    Commenti sul test

--- Calzavara/test_prova.py
+++ Calzavara/test_prova.py
@@ -1,5 +1,5 @@
 from jsonschema import validate
-
+#lezione 3
 schema = {
     "type" : "object",
     "properties" : {
@@ -19,12 +19,14 @@ def bool_validate(instance, schema):
 def test_json():
     assert bool_validate(instance={"name" : "Eggs", "price" : "Invalid"}, schema=schema) == True
 
+def test_success():
+    validate(instance={"name" : "Eggs", "price" : 34.99}, schema=schema)
 
+#Lezione 2
 def func(x):
     return x+1
 
 def test_answer():
     assert func(3) == 4
 
-def test_success():
-    validate(instance={"name" : "Eggs", "price" : 34.99}, schema=schema)
+
