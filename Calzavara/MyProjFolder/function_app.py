c1a0a622df3a7a911f1240b3519b393cee903b4b
accountforfun <fabio.calzavara@itsrizzoli.it>
2024-12-02 13:09:27 +0000

    Pulizia

--- Calzavara/MyProjFolder/function_app.py
+++ Calzavara/MyProjFolder/function_app.py
@@ -3,64 +3,32 @@ import math
 import requests
 import logging
 
-#indirizzo1=Piazza+del+Duomo,+Milano,+Italia&indirizzo2=Colosseo,+Roma,+Italia
-def cal_distanza(lat1, lon1, lat2, lon2):
-    R = 6371.0
-    lat1, lon1, lat2, lon2 = 45.4641, 9.1916, 41.8902, 12.4922 #map(math.radians, [lat1, lon1, lat2, lon2])
-    
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-    
-    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    
-    distanza = R * c
-    return distanza
-
-
-#def coordinate_reali(indirizzo):
-    #url = f'https://nominatim.openstreetmap.org/search?q={indirizzo}&format=json&addressdetails=1'
-    #response = requests.get(url)
-    #data = response.json()
-
-    #if data:
-       
-    #    lat = float(data[0]['lat'])
-    #    lon = float(data[0]['lon'])
-    #    return lat, lon
-    #else:
-    #    return None, None
-
 app = func.FunctionApp()
 
 @app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
 def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    logging.info('informazioni utili????')
 
-    
-    indirizzo1 = req.params.get('indirizzo1')
-    indirizzo2 = req.params.get('indirizzo2')
+    name = req.params.get('name')
+    cognome = req.params.get('cognome')
+    eta = req.params.get('eta')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
 
-    if indirizzo1 and indirizzo2:
-        
-        lat1, lon1 = 0,0#coordinate_reali(indirizzo1)
-        lat2, lon2 = 0,0#coordinate_reali(indirizzo2)
-
-        if lat1 is None or lat2 is None:
-            return func.HttpResponse(
-                "Errore nella geocodifica degli indirizzi. Assicurati che gli indirizzi siano corretti.",
-                status_code=400
-            )
-        
-        
-        distanza = cal_distanza(lat1, lon1, lat2, lon2)
-        
-        return func.HttpResponse(
-            f"La distanza tra '{indirizzo1}' e '{indirizzo2}' è di {distanza:.2f} km.",
-            status_code=200
-        )
+    if name:
+        if cognome:
+            if eta:
+                return func.HttpResponse('topo gigio')
+            return func.HttpResponse(f"Hello, {name} {cognome}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-            "Per favore, passa i parametri 'indirizzo1' e 'indirizzo2' nella query string.",
-            status_code=400
+             "ciaoThis HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
         )
--- Calzavara/provatrigger.py
+++ Calzavara/provatrigger.py
@@ -1,3 +1,4 @@
+'''
 import azure.functions as func
 import datetime
 import json
@@ -194,3 +195,73 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
             "Per favore, passa i parametri 'address1' e 'address2' nella query string.",
             status_code=400
         )
+    
+
+
+
+
+
+
+#indirizzo1=Piazza+del+Duomo,+Milano,+Italia&indirizzo2=Colosseo,+Roma,+Italia
+def cal_distanza(lat1, lon1, lat2, lon2):
+    R = 6371.0
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+    
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    
+    distanza = R * c
+    return distanza
+
+
+def coordinate_reali(indirizzo):
+    url = f'https://nominatim.openstreetmap.org/search?q={indirizzo}&format=json&addressdetails=1'
+    response = requests.get(url)
+    data = response.json()
+
+    if data:
+       
+        lat = float(data[0]['lat'])
+        lon = float(data[0]['lon'])
+        return lat, lon
+    else:
+        return None, None
+
+app = func.FunctionApp()
+
+@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
+def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    
+    indirizzo1 = req.params.get('indirizzo1')
+    indirizzo2 = req.params.get('indirizzo2')
+
+    if indirizzo1 and indirizzo2:
+        
+        lat1, lon1 = coordinate_reali(indirizzo1)
+        lat2, lon2 = coordinate_reali(indirizzo2)
+
+        if lat1 is None or lat2 is None:
+            return func.HttpResponse(
+                "Errore nella geocodifica degli indirizzi. Assicurati che gli indirizzi siano corretti.",
+                status_code=400
+            )
+        
+        
+        distanza = cal_distanza(lat1, lon1, lat2, lon2)
+        
+        return func.HttpResponse(
+            f"La distanza tra '{indirizzo1}' e '{indirizzo2}' è di {distanza:.2f} km.",
+            status_code=200
+        )
+    else:
+        return func.HttpResponse(
+            "Per favore, passa i parametri 'indirizzo1' e 'indirizzo2' nella query string.",
+            status_code=400
+        )
+
+'''
