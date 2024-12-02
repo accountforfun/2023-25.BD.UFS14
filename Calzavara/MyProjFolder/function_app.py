7532056ca78e91e2cc694f84aeca34e47df0674f
accountforfun <fabio.calzavara@itsrizzoli.it>
2024-12-02 14:01:55 +0000

    azure

--- Calzavara/MyProjFolder/function_app.py
+++ Calzavara/MyProjFolder/function_app.py
@@ -24,11 +24,16 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         if cognome:
             if eta:
-                return func.HttpResponse('topo gigio')
-            return func.HttpResponse(f"Hello, {name} {cognome}. This HTTP triggered function executed successfully.")
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+                tempo_rimanente = 100 - int(eta)
+                if tempo_rimanente <= 0:
+                    return func.HttpResponse('You are already dead')
+                if tempo_rimanente > 95:
+                    return func.HttpResponse('Tooo much')
+                return func.HttpResponse(f'Per arrivare a 100 anni ti mancano {tempo_rimanente} anni')
+            return func.HttpResponse(f"Hello, {name} {cognome}. To know how many years you need to reach 100 years, insert your age.")
+        return func.HttpResponse(f"Hello, {name}. Pleaseeee, insert your surname so we can schedule you and steal all your data")
     else:
         return func.HttpResponse(
-             "ciaoThis HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "Hello, This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
